171a27759fecc2071a72dbd2c8c9984be52b4457
anton128355 <47576921+anton128355@users.noreply.github.com>
2021-02-20 23:56:35 +0200

    Update lab1.py

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -12,24 +12,24 @@ print("""
 a = [2, 4, 6, 8]
 
 x = [[random.randint(1, 20) for i in range(8)], 
-	 [random.randint(1, 20) for i in range(8)], 
-	 [random.randint(1, 20) for i in range(8)]]
+     [random.randint(1, 20) for i in range(8)], 
+     [random.randint(1, 20) for i in range(8)]]
 
 x_matrix = numpy.array([x[0], x[1], x[2]])
 
 Y = [a[0] + (a[1] * x[0][i]) + (a[2] * x[1][i]) + (a[3] * x[2][i]) for i in range(8)]
 
 x0 = [(max(x[0]) + min(x[0])) / 2, 
-	  (max(x[1]) + min(x[1])) / 2, 
-	  (max(x[2]) + min(x[2])) / 2]
+      (max(x[1]) + min(x[1])) / 2, 
+      (max(x[2]) + min(x[2])) / 2]
 
 dx = [x0[0] - min(x[0]),
       x0[1] - min(x[1]), 
       x0[2] - min(x[2])]
 
 xn = [[(x[0][i] - x0[0]) / dx[0] for i in range(8)],
-	  [(x[1][i] - x0[1]) / dx[1] for i in range(8)],
-	  [(x[2][i] - x0[2]) / dx[2] for i in range(8)]]
+      [(x[1][i] - x0[1]) / dx[1] for i in range(8)],
+      [(x[2][i] - x0[2]) / dx[2] for i in range(8)]]
 
 xn_matrix = numpy.array([xn[0], xn[1], xn[2]])
 
